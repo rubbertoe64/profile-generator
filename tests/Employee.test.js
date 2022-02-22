9b885bdf1ca42552b97f64e7a24c1b7ae047f365
Roberto Gonzalez <rgonzalez7359@gmail.com>
2022-02-21 18:35:33 -0800

    added roles needed for inquirer answers and exported them

---
+++ tests/Employee.test.js
@@ -0,0 +1,14 @@
+
+const Employee = require('../lib/Employee')
+const newEmployee = new Employee('Robert', 12, "test@gmail.com" )
+describe('Employee', () => {
+    it('should return name', () => {
+        expect(newEmployee.getName()).toBe('Robert')
+    });
+    it('should return id', () => {
+        expect(newEmployee.getId()).toBe(12)
+    });
+    it('should return email', () => {
+        expect(newEmployee.getEmail()).toBe('test@gmail.com')
+    });
+})
